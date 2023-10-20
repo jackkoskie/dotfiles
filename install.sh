ab73f6da7a4be0ea1e1d1259f6ecdd62cbe951bb
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2023-10-19 21:04:48 -0400

    fix: change to unstable branch of nvim

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:neovim-ppa/stable -y
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt-get update -y
 sudo apt-get install neovim -y
