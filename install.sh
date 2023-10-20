cb76b1c390b55add2b589914fb67ba14a0bed169
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2023-10-19 21:15:14 -0400

    feat: add .config linking

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt-get update -y
 sudo apt-get install neovim -y
+
+ln -s ./.config/* ~/.config/
