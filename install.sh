6e3cd91b76a4725a107ad283396de9caf747d1ea
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2023-11-12 01:04:18 +0000

    feat: add ni

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# Setup NeoVim and NvChad
 sudo apt install build-essential -y
 
 sudo apt-get install software-properties-common -y
@@ -6,3 +7,6 @@ sudo apt-get update -y
 sudo apt-get install neovim -y
 
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+
+# Setup ni
+npm install -g @antfu/ni
