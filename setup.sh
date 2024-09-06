f960867f6ecd250c8d112dc668e36c3d000b8c52
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2024-09-05 22:34:39 -0400

    fix issue with needing to accept during install

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Install Neovim
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
-sudo apt install make gcc ripgrep unzip git xclip neovim
-sudo apt-get install ripgrep
+sudo apt install make gcc ripgrep unzip git xclip neovim -y
+sudo apt-get install ripgrep -y
 git clone https://github.com/jackkoskie/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
 # Install Tmux
-sudo apt install tmux
+sudo apt install tmux -y
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Copy dotfiles
